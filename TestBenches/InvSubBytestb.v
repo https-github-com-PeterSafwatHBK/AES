884674bc69f259269faabeb40b1384d7a749e334
sarah-kamal <118070344+sarah-kamal@users.noreply.github.com>
2023-05-04 01:23:31 +0300

    Update InvSubBytestb.v [0:n]

--- TestBenches/InvSubBytestb.v
+++ TestBenches/InvSubBytestb.v
@@ -2,8 +2,8 @@
 
 module InvSubBytestb();
 //use any value for input ,I used value in page (37) round num [4]
-  reg [127:0] in0 = 128'hb415f8016858552e4bb6124c5f998a4c;
-  wire [127:0] out0;
+  reg [0:127] in0 = 128'hb415f8016858552e4bb6124c5f998a4c;
+  wire [0:127] out0;
 
   InvSubBytes mod (.msg(in0), .msgout(out0));
 
